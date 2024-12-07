1cbba7575d7a198f20f994c1652f0bc31b01042f
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-12-07 13:52:48 +0100

    Updated MainActivity.kt.

--- app/src/main/java/com/android/snake2025/MainActivity.kt
+++ app/src/main/java/com/android/snake2025/MainActivity.kt
@@ -2,6 +2,8 @@ package com.android.snake2025
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private val rows: MutableList<LinearLayout> = ArrayList()
 
     private val tileSize = 20
+    private var snakeX = tileSize / 2
+    private var snakeY = tileSize / 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -24,6 +28,23 @@ class MainActivity : AppCompatActivity() {
 
         // TODO generate game board according to tileSize in code
         initBoard()
+        gameLoop()
+    }
+
+    private fun gameLoop() {
+        val mainHandler = Handler(Looper.getMainLooper())
+        mainHandler.post(object  : Runnable {
+            override fun run() {
+                drawBoard()
+                mainHandler.postDelayed(this, 500)
+            }
+        })
+    }
+
+    private fun drawBoard() {
+        // TODO set background color for cells
+        // TODO set background color for snake
+        // TODO move snake in one direction
     }
 
     private fun initBoard() {
