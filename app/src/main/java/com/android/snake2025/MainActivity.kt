e7808598e817b35fd1dba5af6dae93a83c07a8f3
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-12-07 13:31:34 +0100

    Updated MainActivity.kt.

--- app/src/main/java/com/android/snake2025/MainActivity.kt
+++ app/src/main/java/com/android/snake2025/MainActivity.kt
@@ -2,17 +2,13 @@ package com.android.snake2025
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Random
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var layoutMain: LinearLayout
-
-    private val rows: MutableList<LinearLayout> = ArrayList()
 
     private val tileSize = 10
 
@@ -22,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         layoutMain = findViewById(R.id.main)
+
         // TODO generate game board according to tileSize in code
         initBoard()
     }
@@ -35,26 +32,7 @@ class MainActivity : AppCompatActivity() {
             )
             layoutParams.weight = 1f
             row.layoutParams = layoutParams
-
             layoutMain.addView(row)
-            rows.add(row)
-
-            for (j in 0 until tileSize) {
-                val cell = View(this)
-                val layoutParamsCell = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-                )
-                layoutParamsCell.weight = 1f
-                cell.layoutParams = layoutParamsCell
-
-                val random = Random()
-                val color = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
-                cell.setBackgroundColor(color)
-                row.addView(cell)
-            }
         }
-
-        rows.reverse()
     }
 }
