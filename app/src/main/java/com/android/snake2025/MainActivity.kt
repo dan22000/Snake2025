d231b190c908e0dbd82308a1071aa32daea88580
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-12-14 10:29:17 +0100

    Updated MainActivity.kt.

--- app/src/main/java/com/android/snake2025/MainActivity.kt
+++ app/src/main/java/com/android/snake2025/MainActivity.kt
@@ -4,25 +4,34 @@ import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.atan2
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnTouchListener, GestureDetector.OnGestureListener {
 
     private lateinit var layoutMain: LinearLayout
+    private lateinit var gestureDetector: GestureDetector
+
     private val rows: MutableList<LinearLayout> = ArrayList()
     private val tileSize = 20
     private var snakeX = tileSize / 2
     private var snakeY = tileSize / 2
+    private var snakeDirectionX = 0
+    private var snakeDirectionY = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        gestureDetector = GestureDetector(this, this)
         layoutMain = findViewById(R.id.main)
+        layoutMain.setOnTouchListener(this)
 
         initBoard()
         gameLoop()
@@ -32,6 +41,9 @@ class MainActivity : AppCompatActivity() {
         val mainHandler = Handler(Looper.getMainLooper())
         mainHandler.post(object  : Runnable {
             override fun run() {
+                snakeX += snakeDirectionX
+                snakeY += snakeDirectionY
+
                 drawBoard()
                 mainHandler.postDelayed(this, 500)
             }
@@ -39,9 +51,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun drawBoard() {
-        // TODO set background color for cells
-        // TODO set background color for snake
-        // TODO move snake in one direction
         for (x in 0 until tileSize) {
             for (y in 0 until tileSize) {
                 var color = Color.BLACK
@@ -52,8 +61,6 @@ class MainActivity : AppCompatActivity() {
                 rows[y].getChildAt(x).setBackgroundColor(color)
             }
         }
-
-        snakeX--
     }
 
     private fun initBoard() {
@@ -74,5 +81,67 @@ class MainActivity : AppCompatActivity() {
         }
 
         rows.reverse()
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        return gestureDetector.onTouchEvent(event!!)
+    }
+
+    override fun onDown(e: MotionEvent): Boolean {
+        return true
+    }
+
+    override fun onShowPress(e: MotionEvent) {
+    }
+
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
+        return true
+    }
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent, p2: Float, p3: Float): Boolean {
+        val angle = Math.toDegrees(atan2(p0!!.y - p1.y, p1.x - p0.x).toDouble()).toFloat()
+
+        // User swiped Right
+        if (angle > -45 && angle <= 45 && snakeDirectionX == 0) {
+            snakeDirectionY = 0
+            snakeDirectionX = 1
+            return true
+        }
+
+        // User swiped Left
+        if ((angle >= 135 && angle < 180 || angle < -135 && angle > -180) && snakeDirectionX == 0) {
+            snakeDirectionY = 0
+            snakeDirectionX = -1
+            return true
+        }
+
+        // User swiped Down
+        if (angle < -45 && angle >= -135 && snakeDirectionY == 0) {
+            snakeDirectionY = -1
+            snakeDirectionX = 0
+            return true
+        }
+
+        // User swiped Up
+        if (angle > 45 && angle <= 135 && snakeDirectionY == 0) {
+            snakeDirectionY = 1
+            snakeDirectionX = 0
+            return true
+        }
+
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        return true
     }
 }
